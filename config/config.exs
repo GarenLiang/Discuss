20466350ea959f6ff10afc0f5617ee747c1924e2
JackLiang1 <ideabreeze@gmail.com>
2017-02-14 21:52:38 -0600

    Discussion Forum

---
+++ config/config.exs
@@ -0,0 +1,36 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "BnwNE/joMGMjDWEfMXTLhDCeN8SdL3lVxcDo3zQecBCC5W3UpWpE7UqjY2SAfD1o",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "e529b12e88c70db43de0",
+  client_secret: "87262cbb57aba4f008942a33e1f58fa058d0d3cc"
---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :discuss, Discuss.Endpoint,
+  secret_key_base: "l+y5o92TrmlO5kQ2Z6MnfKFA2fFXjQq1ben5auvE9SmHF+D9xX2/mZ3lb3RJFFSL"
+
+# Configure your database
+config :discuss, Discuss.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "discuss_prod",
+  pool_size: 20
